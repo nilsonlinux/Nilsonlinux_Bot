fd9aa4016832c9c99dc1c02157e5adcf3ae69469
Nilsonlinux <nilsonlinux@gmail.com>
2018-12-18 23:40:48 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,27 @@
+'use strict'
+const Telegram = require('telegram-node-bot')
+const TelegramBaseController = Telegram.TelegramBaseController
+const TextCommand = Telegram.TextCommand
+const chatbot = new Telegram.Telegram('703007776:AAGAjCw7YbemwMPpf3mlOKNblmHj1q2idmI')
+
+
+'use strict'
+const Telegram = require('telegram-node-bot')
+const TelegramBaseController = Telegram.TelegramBaseController
+const TextCommand = Telegram.TextCommand
+const chatbot = new Telegram.Telegram('703007776:AAGAjCw7YbemwMPpf3mlOKNblmHj1q2idmI')
+class EventsController extends TelegramBaseController {
+  allEventsAction(scope) {
+    let msg = `QConSP - 24-25-26/04/2017 - qconsp.com\nFrontInSampa - 01/07/2017 - frontinsampa.com.br`
+scope.sendMessage(msg)
+  }
+get routes() {
+    return {
+      'buscacep': 'buscacepAction'
+    }
+  }
+}
+chatbot.router
+       .when(
+         new TextCommand('/buscacep', 'buscacep'), new EventsController()
+       )

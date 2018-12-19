a2be030efae53b9f7a67a0914c0becc892c3ecc9
Nilsonlinux <nilsonlinux@gmail.com>
2018-12-18 23:53:38 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -3,13 +3,6 @@ const Telegram = require('telegram-node-bot')
 const TelegramBaseController = Telegram.TelegramBaseController
 const TextCommand = Telegram.TextCommand
 const chatbot = new Telegram.Telegram('703007776:AAGAjCw7YbemwMPpf3mlOKNblmHj1q2idmI')
-
-
-'use strict'
-const Telegram = require('telegram-node-bot')
-const TelegramBaseController = Telegram.TelegramBaseController
-const TextCommand = Telegram.TextCommand
-const chatbot = new Telegram.Telegram('703007776:AAGAjCw7YbemwMPpf3mlOKNblmHj1q2idmI')
 class EventsController extends TelegramBaseController {
   allEventsAction(scope) {
     let msg = `QConSP - 24-25-26/04/2017 - qconsp.com\nFrontInSampa - 01/07/2017 - frontinsampa.com.br`
